479870a38f79d600d689e4d2c5caa87119909465
Erik Danilo <eriikd@hotmail.com>
2021-06-20 13:29:59 -0300

    Página NotFound criada.

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="a">NotFoundPage</div>
+  <div class="main">
+    <h1 class="msgError">Oops!</h1>
+    <h1>404</h1>
+    <h2>Page NotFound</h2>
+    <p>Desculpe, essa página não pôde ser encontrada!</p>
+  </div>
 </template>
 
 <script>
@@ -8,4 +13,47 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  .msgError {
+    font-size: 150px;
+    background: url(../assets/errorBG.jpg);
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  h1 {
+    font-size: 100px;
+    margin-bottom: 10px;
+  }
+  h2 {
+    font-size: 40px;
+    margin-bottom: 10px;
+  }
+  p {
+    font-size: 20px;
+  }
+}
+@media (max-width: 500px) {
+  .main {
+    .msgError {
+      font-size: 100px;
+    }
+    h1 {
+      font-size: 80px;
+      margin-bottom: 10px;
+    }
+    h2 {
+      font-size: 30px;
+      margin-bottom: 10px;
+    }
+    p {
+      font-size: 17px;
+    }
+  }
+}
+</style>
